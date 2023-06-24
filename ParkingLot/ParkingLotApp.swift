5017f6463283b159ab4b9f83b7d79533c2491e2f
Gagan Singh <gagansingh89@gmail.com>
2023-06-24 11:53:01 -0400

    Initial Commit

---
+++ ParkingLot/ParkingLotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingLotApp.swift
+//  ParkingLot
+//
+//  Created by Gagan Singh on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ParkingLotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
